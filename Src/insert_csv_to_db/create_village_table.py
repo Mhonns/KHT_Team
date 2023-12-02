45c9ec2a150188e8988b233b15e425f1e28e957b
zermisto <pdangkaj@cmkl.ac.th>
2023-12-03 02:12:11 +0700

    made new projectvillage and village table create

--- Src/insert_csv_to_db/create_village_table.py
+++ Src/insert_csv_to_db/create_village_table.py
@@ -17,7 +17,7 @@ columns_to_select = ['Village Name', 'Households', 'Road Conditions', 'Total Pop
     'Distance to Town (km)', 'Adult Males', 'Adult Females', 'Distance to Hospital (km)',
     'Nearest Health Centre', 'Distance to Pratom (km)', 'Annual Typhoid Cases',
     'Distance to Health Centre (km)', 'Distance to Mathayom (km)', 'Common Diseases',
-    'Hosted KHT Projects']
+    'Hosted KHT Projects', 'Record Id']
 
 select_columns_and_save_csv(input_file_path, output_file_path, columns_to_select)
 
@@ -29,7 +29,7 @@ def create_village_table():
 
         crsc = connection.cursor()
        
-        CREATE_TABLE = """CREATE TABLE IF NOT EXISTS village_001 (
+        CREATE_TABLE = """CREATE TABLE IF NOT EXISTS villagetest2 (
                 id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
                 village_name VARCHAR(256),
                 households INTEGER,
@@ -49,7 +49,8 @@ def create_village_table():
                 distance_to_health_centre_km DOUBLE PRECISION,
                 distance_to_mathayom_km DOUBLE PRECISION,
                 common_diseases VARCHAR(256),
-                hosted_kht_projects VARCHAR(256)
+                hosted_kht_projects VARCHAR(256),
+                record_id VARCHAR(256)
             );"""
         crsc.execute(CREATE_TABLE)
         connection.commit()
@@ -58,10 +59,24 @@ def create_village_table():
         with open(output_file_path, 'r') as f:
             next(f)
             crsc.copy_expert(
-                "COPY village_001 (village_name, households, road_conditions, total_population, gps_latitude, gps_longitude, population_without_enough_rice, children_aged_0_18, distance_to_town_km, adult_males, adult_females, distance_to_hospital_km, nearest_health_centre, distance_to_pratom_km, annual_typhoid_cases, distance_to_health_centre_km, distance_to_mathayom_km, common_diseases, hosted_kht_projects) FROM STDIN WITH CSV HEADER",
+                "COPY villagetest2 (village_name, households, road_conditions, total_population, gps_latitude, gps_longitude, population_without_enough_rice, children_aged_0_18, distance_to_town_km, adult_males, adult_females, distance_to_hospital_km, nearest_health_centre, distance_to_pratom_km, annual_typhoid_cases, distance_to_health_centre_km, distance_to_mathayom_km, common_diseases, hosted_kht_projects, record_id) FROM STDIN WITH CSV HEADER",
             f
         )
         connection.commit()    
+
+        # Remove any rows where latitude or longitude is null
+        crsc.execute("""DELETE FROM villagetest2 WHERE gps_latitude IS NULL OR gps_longitude IS NULL;""")
+        connection.commit()
+
+        # add a geom column to the table with a point geometry type from columns gps_latitude and gps_longitude
+        crsc.execute("""ALTER TABLE villagetest2
+            ADD COLUMN geom geometry(Point, 4326);""")
+        connection.commit()
+
+        # update the geom column with the point geometry type from columns gps_latitude and gps_longitude
+        crsc.execute("""UPDATE villagetest2
+            SET geom = ST_SetSRID(ST_MakePoint(gps_longitude, gps_latitude), 4326);""")
+        connection.commit()
 
         print('Village table created successfully.')
     except (Exception, psycopg2.DatabaseError) as error:

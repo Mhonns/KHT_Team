3d5ab81af8bda04a21e1be2511f5d0fdd549b87c
Mhonns <nsamaira@cmkl.ac.th>
2024-02-08 03:57:17 +0000

    add the domain and update the api_provider2 to test the cookie and more

--- Src/api_provider2.py
+++ Src/api_provider2.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, HTTPException, Response
-from fastapi.responses import JSONResponse
+from fastapi.responses import JSONResponse, PlainTextResponse
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import postgreSQL
@@ -34,7 +34,7 @@ def read_root():
     return {"message": "The data hosting is working!"}
 
 @app.get("/api/get/")
-def pull_static_data(table: str, private_key="", village_id=""):
+def pull_static_data(table: str, village_id=""):
     if table == "project":
         json_data = postgreSQL.get_table(table, False, village_id)
         return json_data
@@ -53,8 +53,15 @@ def get_auth(response : Response, username: str, key: str):
     response.set_cookie(key="Username", value=generate_password("1234", 20))
     return {"message": "Come to the dark side, we have cookies"}
 
+@app.get("/.well-known/pki-validation/78AE459F1B584324BF32399DA646034B.txt")
+async def get_txt_file():
+    file_name = "78AE459F1B584324BF32399DA646034B.txt"
+    file_path = f"./{file_name}"
+    file = open(file_path, "r")
+    return PlainTextResponse(file.read())
+
 if __name__ == "__main__":
     import uvicorn
     host = '0.0.0.0'  # '0.0.0.0' to bind to all available network interfaces
-    port = 443  # Change this to your desired port
+    port = 80  # Change this to your desired port
     uvicorn.run(app, host=host, port=port)

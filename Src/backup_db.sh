1cb92fba421cfd03ac1b1bd6860118ff9b3b9da8
zermisto <pdangkaj@cmkl.ac.th>
2024-03-14 10:38:19 +0700

    updated backup_db.sh

--- Src/backup_db.sh
+++ Src/backup_db.sh
@@ -1,28 +1,24 @@
 #!/bin/bash
 
-# Define backup directory (modify as needed)
+# Define backup and log directory
 BACKUP_DIR=~/backups
-
-# Create backup directory if it doesn't exist
-mkdir -p "$BACKUP_DIR"
-
-# Get today's date in YYYY-MM-DD format
-TODAY=$(date +%Y-%m-%d)
-
-# Define backup filename with date
-BACKUP_FILE="$BACKUP_DIR/mhs_geographic_backup_$TODAY.sql"
-
-# Define log directory
 LOG_DIR=~/logs
 
-# Create log directory if it doesn't exist
+# Create backup and log directory 
+mkdir -p "$BACKUP_DIR"
 mkdir -p "$LOG_DIR"
 
+# Get today's date in YYYY-MM-DD format
+TODAY_DATE=$(date +%Y-%m-%d) 
+
+# Define backup filename with date
+BACKUP_FILE="$BACKUP_DIR/mhs_geographic_backup_$TODAY_DATE.sql"
+
 # Define log filename with date
-LOG_FILE="$LOG_DIR/mhs_geographic_backup_$TODAY.log"
+LOG_FILE="$LOG_DIR/mhs_geographic_backup_$TODAY_DATE.log"
 
 # Perform the backup and log to file
 {
   pg_dump -U postgres mhs_geographic > "$BACKUP_FILE" &&
   echo "$(date +'%Y-%m-%d %H:%M:%S') Database backup to $BACKUP_FILE completed."
-} >> "$LOG_FILE" 2>&1
+} >> "$LOG_FILE" 2>&1 # write the stderr and stdout to the log file

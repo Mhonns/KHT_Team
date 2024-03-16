ab39069325d697019c26e2faba9370d1f64508ba
zermisto <pdangkaj@cmkl.ac.th>
2024-03-16 12:45:06 +0700

    Update create_village_url_table.py

--- Src/database/create_village_url_table.py
+++ Src/database/create_village_url_table.py
@@ -9,7 +9,7 @@ def get_file_path(filename):
     file_path = os.path.join(current_dir, filename)
     return file_path
 
-def create_village_table():
+def create_village_url_table():
     try:    
         params = config()
         print('Connecting to the PostgreSQL database...')

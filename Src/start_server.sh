56e83883020d5c98670738e992ab1ac5a20bb6d2
Mhonns <nsamaira@cmkl.ac.th>
2024-03-26 15:59:16 +0700

    start server can now run two server at the same time

--- Src/start_server.sh
+++ Src/start_server.sh
@@ -8,12 +8,30 @@ if ps -p $pid > /dev/null; then
 else
     echo "Process $pid not found"
 fi
+echo "--Successfully kill old server--"
 
 # Starting new server 
-rm nohup.out
+rm -f nohup.out
 nohup python3 api_provider2.py &
 echo $! > pid_file.txt
-echo "Start the server host 0.0.0.0 port 443"
+echo "--Successfully create server--"
+echo "Start the server at host 0.0.0.0 port 443"
+
+# Kill old local server
+local_pid=$(cat local_pid_file.txt)
+if ps -p $local_pid > /dev/null; then
+    kill $local_pid
+else
+    echo "Process $local_pid not found"
+fi
+echo "--Successfully kill old local server--"
+
+# Starting new local server
+rm -f local_nohup.out
+nohup python3 api_provider2.py '127.0.0.1' '2546' &> local_nohup.out &
+echo $! > local_pid_file.txt
+echo "--Successfully create local server--"
+echo "Start the local server at host 127.0.0.1 port 2546"
 
 # Swith the directory back
 cd ../..

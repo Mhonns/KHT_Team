ec0dee792efac7e89bc9cc1e10e4519cec000ec6
zermisto <pdangkaj@cmkl.ac.th>
2024-03-27 16:42:58 +0700

    Update postgreSQL.py

--- Src/postgreSQL.py
+++ Src/postgreSQL.py
@@ -255,7 +255,7 @@ def get_village_from_distance(distance, facility_type="", facility_name=""):
     # facility_type = "hospital"
     # facility_name = "โรงพยาบาลส่งเสริมสุขภาพตำบลสล่าเชียงตอง"
     # distance = 5000
-    print('attempt')
+    print('attempt2')
     print(distance)
     print(facility_type)
     print(facility_name)
